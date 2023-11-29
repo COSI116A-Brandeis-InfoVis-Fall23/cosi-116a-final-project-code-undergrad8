bbd88af79389fe67b384bf4d524b68caf0385067
Eliora Kruman <krumaneliora@gmail.com>
2023-11-28 19:35:59 -0500

    rendered state data, joined our data with the geodata, created basic tooltip

--- js/map.js
+++ js/map.js
@@ -4,9 +4,9 @@ var projection = d3.geoAlbersUsa()    //resize so it can focus in on new england
     .translate([width/500, height/1.73])
     .precision(.1);
 
-var svgStates = d3.select("svg #states");
+var svgStates = d3.select("svg #states");   //select svg
 
-var path = d3.geoPath()
+var path = d3.geoPath() //set path
     .projection(projection);
 
 d3.json("../data/states.json", function(error, topologies) {
@@ -20,11 +20,40 @@ d3.json("../data/states.json", function(error, topologies) {
     console.log(newEnglandData) //debugging
 
     svgStates.selectAll("path")
-        .data(newEnglandData)
+        .data(newEnglandData)   //use the newEngland states only
         .enter()
         .append("path")
-        .attr("d", path)
-        .append("svg:title")
-        .text(function(d) { return d.properties.STATENAM; });   // for some reason it thinks maine is massachusetts, something with the data
-  });
+        .attr("d", path);
+  
+        d3.json("../data/state_local_2021.json", function(error, localCensus2021) {  //nested: join state_local data file to geo data
 
+            var mergeData = newEnglandData.map(function(newEnglandState) {  //
+                var censusState = localCensus2021.find(function(localCensus) {
+                    return localCensus.STATENAM === newEnglandState.properties.STATENAM; //compare the STATENAM property from the geo new england data and the census data
+                });
+
+                return Object.assign({}, newEnglandState, censusState); //assign properties of censusState to newEnglandState
+            });
+
+            console.log(mergeData)  //debugging
+        
+            var paths = svgStates.selectAll("path") //create new paths SVG selection 
+                .data(mergeData)
+                .enter()
+                .append("path")
+                .attr("d", path);
+            paths.merge(svgStates.selectAll("path"))    //use d3 merge function to combine original path with new paths. documentation: https://observablehq.com/@d3/d3-merge
+                .append("svg:title")
+                .text(function(d) { //tooltip
+                    console.log(d.Population)   //debugging
+                    return ["State: " + d.properties.STATENAM + "\nPopulation: " + d.Population + "\nPolice per capita: " +d.Police_per_capita + "\nTotal police expenditure: " + d.Police_protection];
+                });
+                    //TODO: Add color
+                    //TODO: make the tooltip fancy - should it be a separate object as opposed to the way we learned it in class, or is css enough?
+                    //TODO: make map fit page better
+                    //TODO: find state data that isn't from the 1790 when maine was a part of massachusetts lol
+        });
+  
+    });
+
+  
