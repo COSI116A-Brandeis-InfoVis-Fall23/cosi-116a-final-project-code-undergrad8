45f06787cf7607525e7185685aa46719b29598df
GrantGZS <zhongshugu@brandeis.edu>
2023-11-30 16:54:37 -0500

    all scatterplots

--- js/char.js
+++ js/char.js
@@ -1,22 +1,31 @@
 
-fetch('data/21NewEngland.json')
+fetch('../data/21NewEngland.json')
     .then(response => response.json())
     .then(data => {
         // Now 'data' contains your JSON data
+        console.log(data);
         const processedData=processData(data,'Population','Police Protection');
-        console.log(processedData);
-        drawScatterPlot(processedData); // Call a function to draw the scatter plot
+        drawScatterPlot(processedData,'scatterplot-1','Population','Police Protection'); // Call a function to draw the scatter plot
+        const processedData_second=processData(data,'Average income','Police Protection');
+        drawScatterPlot(processedData_second,'scatterplot-2','Average income','Police Protection');
+        const processedData_third=processData(data,'State Revenue','Police Protection');
+        console.log(processedData_third);
+        drawScatterPlot(processedData_third,'scatterplot-3','State Revenue','Police Protection');
+        const processedData_forth=processData(data,'Local Revenue','Police Protection');
+        console.log(processedData_forth);
+        drawScatterPlot(processedData_forth,'scatterplot-4','Local Revenue','Police Protection');
     })
     .catch(error => console.error('Error loading JSON data:', error));
-function drawScatterPlot(data) {
+function drawScatterPlot(data,svgId,xLabel,yLabel) {
 
     // Set the dimensions and margins of the graph
     const margin = { top: 10, right: 30, bottom: 30, left: 60 },
           width = 460 - margin.left - margin.right,
           height = 400 - margin.top - margin.bottom;
-
+    //clear the previous svg content
+    d3.select("#"+svgId).html("");
     // Append the svg object to your page
-    const svg = d3.select("#scatterplot")
+    const svg = d3.select("#"+svgId)
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -27,7 +36,7 @@ function drawScatterPlot(data) {
         .domain([0, d3.max(data, d => d.x)])
         .range([0, width]);
     const xAxis = d3.axisBottom(x)
-      .ticks(7); // Adjust the number of ticks as needed
+      .ticks(5); // Adjust the number of ticks as needed
     
     svg.append("g")
       .attr("class", "x axis")
@@ -51,9 +60,29 @@ function drawScatterPlot(data) {
         .attr("cy", d => y(d.y))
         .attr("r", 2.5)
         .style("fill", "#FF0000");
+    // Array of labels corresponding to the first six data points
+    const labels = ['Connecticut', 'Maine', 'Massachusetts', 'New Hampshire', 'Rhode Island', 'Vermont'];
 
+    // Add labels to the first six data points
+    svg.append('g')
+       .selectAll("text")
+       .data(data.slice(0, 6)) // Take only the first six elements
+       .enter()
+       .append("text")
+       .attr("x", d => x(d.x) + 5) // Position text right of the dot
+       .attr("y", d => y(d.y) - 5) // Position text above the dot
+       .text((d, i) => labels[i]) // Use the index to get the corresponding label
+       .attr("font-size", "10px")
+       .attr("fill", "black");
 
-
+    // Add title to the scatterplot
+    svg.append("text")
+        .attr("x", (width + margin.left + margin.right) / 2)
+        .attr("y", margin.top)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("text-decoration", "underline")
+        .text(`${xLabel} vs ${yLabel}`);
 
 }
 
