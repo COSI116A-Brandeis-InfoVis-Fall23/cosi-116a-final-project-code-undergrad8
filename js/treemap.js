841f989d84e07f452c8c57cc0a24afbbbd4d5be9
Rsirp0c <haorancheng@brandeis.edu>
2023-12-01 21:34:33 -0500

    added treemap, needs further adjust for layout

--- js/treemap.js
+++ js/treemap.js
@@ -1,30 +1,56 @@
-var width = window.innerWidth, 
-        height = window.innerHeight;
+var margin = {top: 10, right: 10, bottom: 10, left: 10},
+  width = 500 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-// Assuming you're using D3 v4 based on your HTML, import CSV data and create a treemap
-d3.dsv(",", "data/tree.csv", d3.autoType).then(data => {
-        let root = d3.hierarchy({values: data}, d => d.values)
-            .sum(d => d.expenditure)
-            .sort((a, b) => b.expenditure - a.expenditure);
-    
-        d3.treemap()
-            .size([width, height])
-            .padding(1)
-            (root);
-    
-        let svg = d3.select("#treemap").select("svg");
-        let g = svg.select("#tree");
-    
-        g.selectAll("rect")
-            .data(root.leaves())
-            .enter()
-            .append("rect")
-            // Example attributes, modify to fit your data and visualization
-            .attr("x", d => d.x0)
-            .attr("y", d => d.y0)
-            .attr("width", d => d.x1 - d.x0)
-            .attr("height", d => d.y1 - d.y0);
-    }).catch(err => {
-        console.log(err);
-    });
-  
+// append the svg object to the body of the page
+d3.select("#vis-2").html("");
+    // Append the svg object to your page
+const svg = d3.select("#vis-2")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Read data
+d3.csv('data/tree.csv', function(data) {
+
+  // stratify the data: reformatting for d3.js
+  var root = d3.stratify()
+    .id(function(d) { return d.name; })   // Name of the entity (column name is name in csv)
+    .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
+    (data);
+  root.sum(function(d) { return +d.value })   // Compute the numeric value for each entity
+
+  // Then d3.treemap computes the position of each element of the hierarchy
+  // The coordinates are added to the root object above
+  d3.treemap()
+    .size([width, height])
+    .padding(6)
+    (root)
+
+console.log(root.leaves())
+  // use this information to add rectangles:
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
+    .enter()
+    .append("rect")
+      .attr('x', function (d) { return d.x0; })
+      .attr('y', function (d) { return d.y0; })
+      .attr('width', function (d) { return d.x1 - d.x0; })
+      .attr('height', function (d) { return d.y1 - d.y0; })
+      // .style("stroke", "black")
+      .style("fill", "#69a4b3");
+
+  // and to add the text labels
+  svg
+    .selectAll("text")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+      .attr("x", function(d){ return d.x0+4})    // +10 to adjust position (more right)
+      .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+      .text(function(d){ return d.data.name})
+      .attr("font-size", "13px")
+      .attr("fill", "white")
+})
