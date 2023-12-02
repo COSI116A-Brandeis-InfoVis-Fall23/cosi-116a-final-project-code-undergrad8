267a7549c016d0211f6fd01177445df75360ea46
Rsirp0c <haorancheng@brandeis.edu>
2023-12-02 14:59:35 -0500

    update treemap

--- js/treemap.js
+++ js/treemap.js
@@ -1,6 +1,6 @@
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
-  width = 500 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  width = 850 - margin.left - margin.right,
+  height = 450 - margin.top - margin.bottom;
 
 // append the svg 
 d3.select("#vis-2").html("");
@@ -8,7 +8,10 @@ const svg = d3.select("#vis-2")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform",
+      // "translate(" + margin.left + "," + margin.top + ")");
+      "translate(" + (width / 4) + "," + (height / 4) + ")"); // Center the <g> element
+
 
 // Read data
 d3.csv('data/tree.csv', function(data) {
@@ -22,8 +25,11 @@ d3.csv('data/tree.csv', function(data) {
 
   d3.treemap()
     .size([width, height])
-    .padding(6)
+    .padding(3)
     (root)
+
+var colorScale = d3.scaleSequential(d3.interpolateBlues)
+  .domain([-0.1,0.6]);
 
 console.log(root.leaves())
   svg
@@ -36,7 +42,10 @@ console.log(root.leaves())
       .attr('width', function (d) { return d.x1 - d.x0; })
       .attr('height', function (d) { return d.y1 - d.y0; })
       // .style("stroke", "black")
-      .style("fill", "#69a4b3");
+      // .style("fill", "#69a4b3");
+      .style("fill", function(d) {
+        return colorScale(d.data.percent);
+     });
 
   // and to add the text labels
   svg
@@ -44,9 +53,20 @@ console.log(root.leaves())
     .data(root.leaves())
     .enter()
     .append("text")
-      .attr("x", function(d){ return d.x0+4})   
+      .attr("x", function(d){ return d.x0+5})   
       .attr("y", function(d){ return d.y0+20})    
       .text(function(d){ return d.data.name})
-      .attr("font-size", "13px")
+      .attr("font-size", "15px")
       .attr("fill", "white")
+
+  svg
+  .selectAll("text.percentage")
+  .data(root.leaves())
+  .enter()
+  .append("text")
+    .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
+    .attr("y", function(d){ return d.y0+37})    // +60 to adjust position (lower)
+    .text(function(d){ return (d.data.percent*100).toFixed(1) + "%" })
+    .attr("font-size", "13px")
+    .attr("fill", "white");
 })
